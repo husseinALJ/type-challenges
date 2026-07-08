21dc768e7d8af014b7e63c9a8cc92b382e481ad7
hussainALJ <149183695+hussainALJ@users.noreply.github.com>
2026-07-08 16:22:49 +0300

    Make a type Constructor by picking set K from T
    Type MyPick of T and K, where K extends keyof T is
     an object mapping every property in the intersection of
      the keys of T and K, to that property type in T

--- playground/easy/00004-easy-pick.ts
+++ playground/easy/00004-easy-pick.ts
@@ -31,7 +31,9 @@
 
 /* _____________ Your Code Here _____________ */
 
-type MyPick<T, K> = any
+type MyPick<T, K extends keyof T> = {
+  [Prop in keyof T & K]: T[Prop];
+}
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
