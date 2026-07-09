41d08a1db45c09e4b9f99ade31a1be099e940fb6
hussainALJ <149183695+hussainALJ@users.noreply.github.com>
2026-07-09 17:56:23 +0300

    Implement type-level Includes function to check for element presence in an array

--- playground/easy/00898-easy-includes.ts
+++ playground/easy/00898-easy-includes.ts
@@ -18,7 +18,8 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Includes<T extends readonly any[], U> = any
+type Includes<T extends readonly any[], U> = 
+  T extends [infer T1, ...infer Rest]? Equal<T1, U> extends true? true: Includes<Rest, U>: false;
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
